25c68a959e320df4009438dd87d32732b5328bea
budou114 <58349030+budou114@users.noreply.github.com>
2025-03-22 14:37:19 +0900

    20. メタデータの設定方法

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -4,6 +4,13 @@ type Params = {
   }>
 }
 
+export async function generateMetadata({params} : Params){
+  const { id } = await params;
+  return {
+    title: `ブログ記事ID: ${id}です`
+  }
+}
+
 export default async function page({params} : Params) {
   // console.log(params);
   const { id } = await params;
