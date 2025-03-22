4afa354d8bf439647203d9acb2e9258bb58cd089
budou114 <58349030+budou114@users.noreply.github.com>
2025-03-22 15:50:17 +0900

    25. RSCとRCCの違い・ReactHook, イベント周り

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,8 +1,13 @@
+// import { useState } from "react"
+
 export default function SeverComponent() {
+  // const [ count, setCount ] = useState(0)
+
   console.log('Server')
   return (
     <div>
       サーバー
+      {/* <button onClick={() => setCount(count + 1)}>Count: { count }</button> */}
     </div>
   )
 }
